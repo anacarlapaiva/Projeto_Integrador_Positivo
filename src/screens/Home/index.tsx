5c50b461cff368777d211f1c8478ff42be7c42ad
Ana Carla Paiva <acpaiva99@outlook.com>
2022-11-16 20:39:34 -0300

    feat: alterações do searchinput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text } from 'react-native'
 import Header from '../../components/Header'
-import { Container } from './styles'
+import { Container, ContentSearch, ContentSubMenu, IconSearch, SearchInput, SeeAll, TitlePage } from './styles'
 
-const Home = () => {
+const Home: React.FC = () => {
+  const [search, setSearch] = useState<string>("");
+
   return (
     <Container>
       <Header />
+      <ContentSearch>
+        <IconSearch name="search" size={24} />
+        <SearchInput
+          placeholderTextColor={"#808B98"}
+          placeholder='Pesquisar'
+          value={search}
+          onChangeText={setSearch}
+          underlineColorAndroid="transparent"
+        />
+      </ContentSearch>
+      <ContentSubMenu>
+        <TitlePage>Escolha sua opção</TitlePage>
+        <SeeAll>Ver todos</SeeAll>
+      </ContentSubMenu>
     </Container>
   )
 }
