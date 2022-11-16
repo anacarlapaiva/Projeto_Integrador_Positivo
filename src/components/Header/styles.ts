dcf23bec27d1798c1a1d890c6ec5b33473c8c980
Ana Carla Paiva <acpaiva99@outlook.com>
2022-11-16 20:06:04 -0300

    style: estilização do header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,30 +1,25 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-// import Image from "../../assets/profile-user.png";
-
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
   padding: 0 18px;
 `;
 
-
 export const HeaderElement = styled.View`
   width: 100%;
   height: 10%;
   flex-direction: row;
   align-items: center;
-  margin-top: ${RFValue(30)}px;
+  justify-content: flex-start;
+  margin-top: ${RFValue(70)}px;
 `;
 
 export const Name = styled.Text`
     color: ${({theme}) => theme.colors.primary};
 `;
 
-// export const Picture = styled(Image)`
-
-// `;
 
 export const Comeback = styled.Text`
   font-size: ${RFValue(24)}px;
