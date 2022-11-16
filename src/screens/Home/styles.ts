a5cc648c9f4ba495c2613ef6653c495a67a0c34b
Ana Carla Paiva <acpaiva99@outlook.com>
2022-11-16 20:06:15 -0300

    style: propriedade padrão da aplicação

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,5 +3,4 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-  padding: 0 18px;
 `;
