1c56b604f7d7b2f1a3588ef5a60b8ab183518313
Ana Carla Paiva <acpaiva99@outlook.com>
2022-11-16 21:55:50 -0300

    feat: ajuste home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,20 @@
-import React, { useState } from 'react'
-import { Text } from 'react-native'
-import Header from '../../components/Header'
-import { Container, ContentSearch, ContentSubMenu, IconSearch, SearchInput, SeeAll, TitlePage } from './styles'
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import {
+  Box,
+  ViewBox,
+  Container,
+  ContentListImage,
+  ContentSearch,
+  ContentSubMenu,
+  IconSearch,
+  SearchInput,
+  SeeAll,
+  TitlePage,
+  TextBottom,
+} from "./styles";
+
+import FotoExemplo from "../../assets/example.png";
 
 const Home: React.FC = () => {
   const [search, setSearch] = useState<string>("");
@@ -13,7 +26,7 @@ const Home: React.FC = () => {
         <IconSearch name="search" size={24} />
         <SearchInput
           placeholderTextColor={"#808B98"}
-          placeholder='Pesquisar'
+          placeholder="Pesquisar"
           value={search}
           onChangeText={setSearch}
           underlineColorAndroid="transparent"
@@ -23,8 +36,27 @@ const Home: React.FC = () => {
         <TitlePage>Escolha sua opção</TitlePage>
         <SeeAll>Ver todos</SeeAll>
       </ContentSubMenu>
-    </Container>
-  )
-}
 
-export default Home
+      <ContentListImage>
+        <ViewBox>
+          <Box source={FotoExemplo} />
+          <TextBottom>Apartamento</TextBottom>
+        </ViewBox>
+        <ViewBox>
+          <Box source={FotoExemplo} />
+          <TextBottom>Apartamento</TextBottom>
+        </ViewBox>
+        <ViewBox>
+          <Box source={FotoExemplo} />
+          <TextBottom>Apartamento</TextBottom>
+        </ViewBox>
+        <ViewBox>
+          <Box source={FotoExemplo} />
+          <TextBottom>Apartamento</TextBottom>
+        </ViewBox>
+      </ContentListImage>
+    </Container>
+  );
+};
+
+export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Dimensions } from "react-native";
+
+const width = Dimensions.get("window");
 
 export const Container = styled.View`
   flex: 1;
@@ -14,8 +17,8 @@ export const ContentSearch = styled.View`
   align-items: center;
   padding: ${RFValue(10)}px;
   border-radius: ${RFValue(8)}px;
-  background-color: #F3F3F3;
-  height:${RFValue(54)}px;
+  background-color: #f3f3f3;
+  height: ${RFValue(54)}px;
 `;
 
 export const IconSearch = styled(Feather)`
@@ -37,12 +40,37 @@ export const ContentSubMenu = styled.View`
 
 export const TitlePage = styled.Text`
   font-size: ${RFValue(16)}px;
-  color: ${({theme}) => theme.colors.title};
+  color: ${({ theme }) => theme.colors.title};
   font-weight: bold;
 `;
 
 export const SeeAll = styled.Text`
   font-size: ${RFValue(14)}px;
-  color: ${({theme}) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
+export const ContentListImage = styled.View`
+  margin-top: ${RFValue(20)}px;
+  flex-direction: row;
+  justify-content: space-between;
+  flex: 1;
+  flex-wrap: wrap;
+`;
+
+export const ViewBox = styled.View`
+  flex-direction: column;
+  flex-basis: 50%;
+`;
+
+export const Box = styled.Image`
+
+
+`;
+
+export const TextBottom = styled.Text`
+  color: #928f8f;
+  font-size: ${RFValue(14)}px;
+  margin-top: ${RFValue(8)}px;
+  margin-bottom: ${RFValue(16)}px;
+
+`;
