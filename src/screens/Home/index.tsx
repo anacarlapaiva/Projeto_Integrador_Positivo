aa4494bb2587772fd75a85010760703fee20666e
Ana Carla Paiva <acpaiva99@outlook.com>
2022-11-17 11:00:47 -0300

    feat: funcionalidade ao menu de seleção de crud

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -16,56 +16,69 @@ import {
 
 import FotoExemplo from "../../assets/example.png";
 import { ScrollView } from "react-native-gesture-handler";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const Home: React.FC = () => {
   const [search, setSearch] = useState<string>("");
 
   return (
-    <Container>
-      <Header />
-      <ContentSearch>
-        <IconSearch name="search" size={24} />
-        <SearchInput
-          placeholderTextColor={"#808B98"}
-          placeholder="Pesquisar"
-          value={search}
-          onChangeText={setSearch}
-          underlineColorAndroid="transparent"
-        />
-      </ContentSearch>
-      <ContentSubMenu>
-        <TitlePage>Escolha sua opção</TitlePage>
-        <SeeAll>Ver todos</SeeAll>
-      </ContentSubMenu>
-      <ScrollView>
-        <ContentListImage>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-          <ViewBox>
-            <Box source={FotoExemplo} />
-            <TextBottom>Apartamento</TextBottom>
-          </ViewBox>
-        </ContentListImage>
-      </ScrollView>
-    </Container>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <Container>
+          <Header />
+          <ContentSearch>
+            <IconSearch name="search" size={24} />
+            <SearchInput
+              placeholderTextColor={"#808B98"}
+              placeholder="Pesquisar"
+              value={search}
+              onChangeText={setSearch}
+              underlineColorAndroid="transparent"
+            />
+          </ContentSearch>
+          <ContentSubMenu>
+            <TitlePage>Escolha sua opção</TitlePage>
+            <SeeAll>Ver todos</SeeAll>
+          </ContentSubMenu>
+          <ScrollView>
+            <ContentListImage>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+              <ViewBox>
+                <Box source={FotoExemplo} />
+                <TextBottom>Apartamento</TextBottom>
+              </ViewBox>
+            </ContentListImage>
+          </ScrollView>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
